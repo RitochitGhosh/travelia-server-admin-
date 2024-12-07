03ee337e6136e38138aa32a3d47af5a8a123eaa4
RitochitGhosh <ghosh.18.ritochit12@gmail.com>
2024-12-07 20:04:47 +0530

    Changes in page.tsx

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,16 +1,24 @@
 import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
-const BillboardPage = async ({
-  params,
-}: {
-  params: { billboardId: string };
-}) => {
+interface BillboardPageProps {
+  params: {
+    billboardId: string;
+  };
+}
+
+const BillboardPage = async ({ params }: BillboardPageProps) => {
+  // Ensure the async data fetching is correctly awaited
   const billboard = await prismadb.billboard.findUnique({
     where: {
       id: params.billboardId,
     },
   });
+
+  if (!billboard) {
+    return <div>Billboard not found</div>;
+  }
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
